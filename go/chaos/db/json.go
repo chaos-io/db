ebc34fb9d61def9e1564e1860f72c7970757ac9e
Eric Yue <yuekewin@gmail.com>
2026-03-05 09:46:29 +0800

    feat: add json value and scan

---
+++ go/chaos/db/json.go
@@ -0,0 +1,59 @@
+package db
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"reflect"
+
+	jsoniter "github.com/json-iterator/go"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+type JSONValuer struct{}
+
+func (v JSONValuer) Value(value any) (driver.Value, error) {
+	if val := reflect.ValueOf(value); val.IsZero() {
+		return nil, nil
+	}
+
+	return jsoniter.ConfigFastest.MarshalToString(value), nil
+}
+
+type JSONScanner struct{}
+
+func (s JSONScanner) Scan(value, src any) error {
+	if src == nil {
+		return nil
+	}
+	if val := reflect.ValueOf(value); !val.IsValid() || (val.CanAddr() && val.IsNil()) {
+		return nil
+	}
+
+	switch v := src.(type) {
+	case []byte:
+		return jsoniter.ConfigFastest.Unmarshal(v, value)
+	case string:
+		return jsoniter.ConfigFastest.Unmarshal([]byte(v), value)
+	default:
+		return fmt.Errorf("failed to unmarshall JSONB value: %v", src)
+	}
+}
+
+type JSONDbDataType struct{}
+
+func (JSONDbDataType) GormDBDataType(db *gorm.DB, field *schema.Field) string {
+	return gormDBJSONType(db)
+}
+
+func gormDBJSONType(db *gorm.DB) string {
+	switch db.Dialector.Name() {
+	case "sqlite":
+		return "JSON"
+	case "mysql":
+		return "JSON"
+	case "postgres":
+		return "JSONB"
+	}
+	return ""
+}
--- go/go.mod
+++ go/go.mod
@@ -2,4 +2,16 @@ module github.com/chaos-io/db/go
 
 go 1.24.12
 
-require google.golang.org/protobuf v1.36.11
+require (
+	github.com/json-iterator/go v1.1.12
+	google.golang.org/protobuf v1.36.11
+	gorm.io/gorm v1.31.1
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	golang.org/x/text v0.20.0 // indirect
+)
